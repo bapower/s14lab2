a15b1b7e1dc85f9d98a620b2985fd506ebd38e62
bry.power <bapower57@gmail.com>
2020-07-07 01:12:05 +0100

    bootstrap and dropdown

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 def index():
     linearRegression = joblib.load('./models/linearRegression.pkl')
     decisionTree = joblib.load('./models/decisionTree.pkl')
+    # Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
     linearRegressionPrediction = str(linearRegression.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1))
-    decisionTreePrediction = str(decisionTree.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1))
+    decisionTreePrediction = str(decisionTree.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0])
     return (render_template('index.html', 
     	linearRegressionPrediction = linearRegressionPrediction, 
     	decisionTreePrediction = decisionTreePrediction))
